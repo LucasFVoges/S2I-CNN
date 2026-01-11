9090645692f018b5334726b1403491fc8f260739
Lucas F. Voges <lucas.filipo@posteo.de>
2026-01-11 20:06:16 +0100

    Update: Data Generation and Streamlit App

--- 02_TestWithSimData/Test_app.py
+++ 02_TestWithSimData/Test_app.py
@@ -14,32 +14,61 @@ from functions.DataGenerator import data_gen
 plt.style.use("dark_background")
 
 @st.cache_data
-def load_data():
-    df = data_gen(n = 3, length=50, resolution=0.5, noise=1, num_peaks=1, seed=42)
+def load_data(n = 10, length=50, resolution=0.5, noise=1, y_shift=0.02, num_peaks=3, peak_randomised_amount= 0.1, seed=12):
+    df = data_gen(n, length, resolution, noise, y_shift, num_peaks, peak_randomised_amount, seed)
     return df
 
 data = load_data()
 
-st.title("S2I-CNN - Test :streamlit:")
+st.title("S2I-CNN - Simulation :streamlit:")
 st.write("An example of how the 'Spectra2Image CNN' can be used for 'simulated' data.")
 
-tab_data, tab_results = st.tabs(["Data" ,"Results"])
+tab_data, tab_S2I, tab_CNN = st.tabs(["Data" ,"S2I", "CNN"])
 
 with tab_data:
     st.header("Data")
-    st.write("The first 10 rows of the generated data:")
-    st.write("3 first spectra:")
-    st.dataframe(data.head(3))
+
+    data_gen_form = st.form("Data Generator")
+    with data_gen_form:
+        num = st.number_input("Number of Spectra per Class:", value=10, step=1, min_value=1, max_value=1000)
+        len = st.number_input("Length of Spectra:", value=50, step=1, min_value=10, max_value=1000)
+        n_peaks = st.number_input("Number of peaks:", value=3, step=1, min_value=1, max_value=100)
+        noi = st.slider("Noise:", 0.0, 10.0, 1.0)
+        y_shifted = st.slider("Shift:", 0.00, 1.00, 0.02)
+        peak_rand = st.slider("Randomised:", 0.0, 1.0, 0.1)
+        rand = st.checkbox("Random Seed")
+        submit = st.form_submit_button("generate new data")
+
+    if submit:
+        if rand:
+            seeded = np.random.randint(1,1000)
+        else:
+            seeded = 12
+        data = load_data(num, len, 0.5, noi, y_shifted, n_peaks, peak_rand, seeded)
 
     fig, ax = plt.subplots()
-    sns.lineplot(data=data.head(3).T, legend=False)
+    sns.lineplot(data=data.head(10).T, legend=False, dashes=False)
     ax.set_xlabel("Wavenumber")
     ax.set_ylabel("Intensity")
+    ax.set_title("Random Spectra Simulation")
     st.pyplot(fig)
 
+    fig, ax = plt.subplots()
+    sns.lineplot(data=data.tail(10).T, legend=False, dashes=False)
+    ax.set_xlabel("Wavenumber")
+    ax.set_ylabel("Intensity")
+    ax.set_title("With Added Peak")
+    st.pyplot(fig)
 
-with tab_results:
-    st.header("Results")
+    st.write("The first 5 and last 5 rows of the generated data:")
+    st.dataframe(data.head(5))
+    st.dataframe(data.tail(5))
+
+with tab_S2I:
+    st.header("S2I")
+
+with tab_CNN:
+    st.header("CNN")
 
 st.divider()
 
--- 02_TestWithSimData/functions/DataGenerator.py
+++ 02_TestWithSimData/functions/DataGenerator.py
@@ -5,13 +5,15 @@ import pandas as pd
 if __name__ == "__main__":
     pass
 
-def data_gen(n = 100, length = 10, resolution = 0.4, noise = 1, num_peaks = 1,  seed = 42):
+def data_gen(n = 100, length = 10, resolution = 0.4, noise = 1, y_shift = 0.02, num_peaks = 1, peak_randomised_amount = 0.05, seed = 42):
     """
     generates a test Dataset
     Including 2 variants of a random spectrum (function).
 
+    # TODO: ADD More Peaks to the second class
+    # TODO: Length should not influence the peak parameter!
 
-    :param n: number of spectra to generate (default: 100, < 2 will be set to 2).
+    :param n: number of spectra to generate for each Class! (default: 100).
     :type n: int
     :param length: number of spectrapoints to generate (default: 10, min: 10).
     :type length: int
@@ -19,8 +21,12 @@ def data_gen(n = 100, length = 10, resolution = 0.4, noise = 1, num_peaks = 1,  
     :type resolution: float
     :param noise: The noise level (default: 1).
     :type noise: float
+    :param y_shift: max amount of y value to be shifted (default: 0.02).
+    :type y_shift: float
     :param num_peaks: number of peaks (default: 1, min: 1 cannot be larger than length/2).
     :type num_peaks: int
+    :param peak_randomised_amount: maximal amount added to the center, amplitude and widht of every peak (default: 0.05).
+    :type peak_randomised_amount: float
     :param seed: random seed for reproducibility (default: 42).
     :type seed: int
     :return: data frame with spectral intensities
@@ -31,7 +37,7 @@ def data_gen(n = 100, length = 10, resolution = 0.4, noise = 1, num_peaks = 1,  
 
     # checks:
     if length < 10: length = 10
-    if n < 2: n = 2
+    if n < 1: n = 1
     if resolution > length: resolution = length
     if num_peaks < 1: num_peaks = 1
     if num_peaks > length/2: num_peaks = length/2
@@ -41,16 +47,27 @@ def data_gen(n = 100, length = 10, resolution = 0.4, noise = 1, num_peaks = 1,  
     x = np.linspace(0, length, num_points)
     spectra = []
 
-    for _ in range(n):
+    # Define the base spectrum parameters
+    amplitudes = []
+    centers = []
+    widths = []
+    for _ in range(num_peaks+1):                                # This can be directly done with np.random.uniform(0,1,num_peaks)
+        amplitudes.append(np.random.uniform(0.1,1))
+        centers.append(np.random.uniform(0,1) * length)
+        widths.append(np.random.uniform(0.2,1) * (length * 0.1) + 0.1)
+
+    for spec in range(n*2):
         # Create a base spectrum as a sum of min and max peaks random Gaussians
         y = np.ones_like(x)
+        y = y + np.random.uniform(-y_shift, y_shift)
 
         # Adds random Gaussian peaks to base spectrum
-        for _ in range(num_peaks):
-            amplitude = np.random.rand()
-            center = np.random.rand() * length
-            width = np.random.rand() * (length * 0.1) + 0.1
-            y += amplitude * np.exp(-((x - center) ** 2) / (2 * width ** 2))
+        range_peaks = num_peaks if spec < n else num_peaks + 1
+        for i in range(range_peaks):
+            this_amplitude = amplitudes[i] + np.random.uniform(-peak_randomised_amount,peak_randomised_amount)
+            this_center = centers[i] + np.random.uniform(-peak_randomised_amount,peak_randomised_amount)
+            this_width = widths[i] + np.random.uniform(-peak_randomised_amount,peak_randomised_amount)
+            y += this_amplitude * np.exp(-((x - this_center) ** 2) / (2 * this_width ** 2))
 
         # Add Gaussian noise
         y += np.random.normal(0, noise/100, size=x.shape)
